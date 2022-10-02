9ee39b853e163ba419dc5f5fefed13362d84bb71
Rogier van der Sluijs <rogier@vandersluijs.tech>
2022-10-02 01:14:12 -0700

    http reader

--- dosma/core/io/http_io.py
+++ dosma/core/io/http_io.py
@@ -7,7 +7,7 @@ This module contains HTTP input/output helpers.
 
 from io import BytesIO
 import zipfile
-from typing import Tuple, Union, List, Dict, Collection, Optional, Iterator
+from typing import Tuple, Union, List, Dict, Collection
 
 import numpy as np
 import requests
@@ -15,9 +15,7 @@ from tqdm.auto import tqdm
 
 from dosma.core.io.dicom_io import DicomReader
 from dosma.core.io.format_io import DataReader
-from dosma.core.io.container_io import ImageContainerFormat
-from dosma.core.io.container_io_utils import get_reader
-from dosma.core.io.http_io_utils import is_url
+from dosma.core.io.http_io_utils import is_url, extract_boundary, extract_part
 
 __all__ = ["HttpReader"]
 
@@ -30,88 +28,91 @@ _MIME_TYPES_ZIP = [
 ]
 
 
-def _extract_part(part: bytes) -> Union[bytes, None]:
-    if part in [b"", b"--", b"\r\n"] or part.startswith(b"--\r\n"):
-        return None
-
-    idx = part.index(b"\r\n\r\n")
-    if idx > -1:
-        return part[idx + 4:]
-
-    raise ValueError("Part is not CRLF CRLF terminated.")
-
-def extract_boundary(content_info: List[str]) -> Optional[bytes]:
-    for item in content_info:
-        if '=' not in item:
-            continue
-
-        key, value = item.split('=', maxsplit=1)
-        if key.lower() == "boundary":
-            return b"--" + value.strip('"').encode("utf-8")
-
-    return None
-
-def read_multipart(
-    res: requests.Response,
-    boundary: bytes,
-) -> Iterator[bytes]:
-
-    delimiter = b"\r\n" + boundary
-    data = b""
-
-    j = 0
-    it = res.iter_content(chunk_size=chunk_size)
-
-    for i, chunk in enumerate(it):
-        data += chunk
-        while delimiter in data:
-            part, data = data.split(delimiter, maxsplit=1)
-            content = _extract_part(part)
-
-            j += 1
-            if content is not None:
-                yield content
-
-    content = _extract_part(data)
-    if content is not None:
-        yield content
-
-
 class HttpReader(DataReader):
     """A class for reading DICOMs from HTTP requests.
 
     Attributes:
         verbose (bool, optional): If ``True``, show loading progress bar.
+        block_size (int, optional): Block size for reading data.
+        **kwargs: Keyword arguments for :class:`DicomReader`.
 
     Examples:
-
+        >>> from dosma.core.io import HttpReader
+        >>> reader = HttpReader()
+        >>> reader.read("https://server.com/dicom.zip")
     """
-
     def __init__(
         self,
         verbose: bool = False,
+        block_size: int = 10 ** 6,
         **kwargs,
     ):
-        """
-        Args:
-            verbose (bool, optional): If ``True``, show loading progress bar.
-        """
-
         self.verbose = verbose
+        self.block_size = block_size
         self.session = requests.Session()
         self.kwargs = kwargs
 
-    def _read_dicom(self, buffers: List[bytes], **kwargs):
-        """Read DICOMs from data."""
+    def _read_multipart_stream(self, res: requests.Response, content_info: str, pbar: tqdm) -> List[bytes]:
+        """Read multipart stream.
 
-        dr = DicomReader(**{**self.kwargs, "verbose": self.verbose, **kwargs})
+        Args:
+            res (requests.Response): Response object.
+            content_info (str): Content info.
+            pbar (tqdm): Progress bar.
+        """
+        boundary = extract_boundary(content_info)
+        blob, parts = bytes(), []
+
+        for block in res.iter_content(self.block_size):
+            pbar.update(len(block))
+
+            blob += block
+            while boundary in blob:
+                part, blob = blob.split(boundary, maxsplit=1)
+                content = extract_part(part)
+
+                if content is not None:
+                    parts.append(content)
+
+        content = extract_part(blob)
+        if content is not None:
+            parts.append(content)
+
+        pbar.close()
+        return parts
+
+    def _read_stream(self, res: requests.Response, pbar: tqdm) -> bytes:
+        """Read stream.
+
+        Args:
+            res (requests.Response): Response object.
+            pbar (tqdm): Progress bar.
+        """
+
+        blob = bytes()
+        for block in res.iter_content(self.block_size):
+            pbar.update(len(block))
+            blob += block
+
+        pbar.close()
+        return blob
+
+    def _read_dicom(self, buffers: List[bytes], **kwargs):
+        """Read DICOMs from data.
+
+        Args:
+            buffers (List[bytes]): List of bytes objects.
+            **kwargs: Keyword arguments for :class:`DicomReader`.
+        """
+
+        # do not pass verbose to reader, as files are already opened
+        dr = DicomReader(**{**self.kwargs, **kwargs})
         return dr.read([BytesIO(buffer) for buffer in buffers])
 
     def load(
         self,
         url: str,
         params: Union[Dict, List[Tuple], bytes] = np._NoValue,
-        block_size: int = 10 ** 6,
         **kwargs,
     ):
         """Load data from HTTP request.
@@ -119,7 +120,7 @@ class HttpReader(DataReader):
         Args:
             url (str): URL.
             params (Union[Dict, List[Tuple], bytes], optional): Parameters to send with the request.
-            **kwargs: Additional keyword arguments passed to the DicomReader.
+            **kwargs: Keyword arguments for :class:`DicomReader`.
         """
 
         if not is_url(url):
@@ -130,7 +131,7 @@ class HttpReader(DataReader):
             content_length = res.headers.get("Content-Length", 0)
             content_type = res.headers.get("Content-Type", "application/octet-stream").lower()
 
-            progress_bar = tqdm(
+            pbar = tqdm(
                 total=content_length,
                 unit="B",
                 unit_scale=True,
@@ -138,82 +139,24 @@ class HttpReader(DataReader):
                 disable=not self.verbose,
             )
 
+            # multipart/related
             if content_type.startswith("multipart/related;"):
                 _, *content_info = [part.strip() for part in content_type.split(";")]
-                boundary = extract_boundary(content_info)
-                blob, parts = bytes(), []
 
-                for block in res.iter_content(block_size):
-                    progress_bar.update(len(block))
-
-                    blob += block
-                    while boundary in blob:
-                        part, blob = blob.split(boundary, maxsplit=1)
-                        content = _extract_part(part)
-
-                        if content is not None:
-                            parts.append(content)
-
-                content = _extract_part(blob)
-                if content is not None:
-                    parts.append(content)
-
-                progress_bar.close()
+                parts = self._read_multipart_stream(res, content_info, pbar)
                 return self._read_dicom(parts, **kwargs)
 
+            # application/zip
             if content_type in _MIME_TYPES_ZIP:
-                blob = BytesIO()
-                for block in res.iter_content(block_size):
-                    progress_bar.update(len(block))
-                    blob.write(block)
-
-                z = zipfile.ZipFile(blob)
+                blob = self._read_stream(res, pbar)
+                z = zipfile.ZipFile(BytesIO(blob))
                 parts = [z.read(zinfo) for zinfo in z.infolist() if zinfo.file_size > 0]
 
-                progress_bar.close()
                 return self._read_dicom(parts, **kwargs)
 
-            blob = bytes()
-            for block in res.iter_content(block_size):
-                progress_bar.update(len(block))
-                blob += block
-
+            # fallback to single file
+            blob = self._read_stream(res, pbar)
             return self._read_dicom([blob], **kwargs)
-
-    # def load(
-    #     self,
-    #     url: str,
-    #     params: Union[Dict, List[Tuple], bytes] = np._NoValue,
-    #     **kwargs,
-    # ):
-    #     """Load data from HTTP request.
-
-    #     Args:
-    #         url (str): URL.
-    #         params (Union[Dict, List[Tuple], bytes], optional): Parameters to send with the request.
-    #         **kwargs: Additional keyword arguments passed to the DicomReader.
-    #     """
-
-    #     if not is_url(url):
-    #         raise IOError(f"Invalid URL: {url}.")
-
-    #     params = params if params != np._NoValue else self.session.params
-    #     with self.session.get(url, params=params, stream=True) as res:
-    #         content_length = res.headers.get("Content-Length", 0)
-    #         content_type = res.headers.get("Content-Type", "application/octet-stream")
-
-    #         if self.verbose:
-    #             res.raw.read = partial(res.raw.read, decode_content=True)
-    #             with tqdm.wrapattr(res.raw, "read", total=content_length) as raw:
-    #                 container_or_file = BytesIO()
-    #                 shutil.copyfileobj(raw, container_or_file)
-
-    #                 return self._handle_blob(container_or_file, content_type, **kwargs)
-    #         else:
-    #             container_or_file = BytesIO()
-    #             shutil.copyfileobj(res.raw, container_or_file)
-
-    #             return self._handle_blob(container_or_file, content_type, **kwargs)
 
     def close(self):
         """Close the current HTTP session."""
@@ -223,7 +166,7 @@ class HttpReader(DataReader):
     def __enter__(self):
         return self
 
-    def __exit__(self):
+    def __exit__(self, *args, **kwargs):
         self.close()
 
     def __serializable_variables__(self) -> Collection[str]:
--- dosma/core/io/http_io_utils.py
+++ dosma/core/io/http_io_utils.py
@@ -1,9 +1,7 @@
 import re
-import zipfile
-from io import BytesIO
-from typing import List
+from typing import List, Union, Optional
 
-__all__ = ["is_url"]
+__all__ = ["is_url", "extract_boundary", "extract_part"]
 
 
 def is_url(url: str) -> bool:
@@ -15,7 +13,6 @@ def is_url(url: str) -> bool:
     Returns:
         bool: Result of the URL validation.
     """
-
     regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain
@@ -25,3 +22,28 @@ def is_url(url: str) -> bool:
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
     return re.match(regex, url) is not None
+
+
+def extract_part(part: bytes) -> Union[bytes, None]:
+    """Extract part from multipart stream."""
+    if part in [b"", b"--", b"\r\n"] or part.startswith(b"--\r\n"):
+        return None
+
+    idx = part.index(b"\r\n\r\n")
+    if idx > -1:
+        return part[idx + 4:]
+
+    raise ValueError("Part is not CRLF CRLF terminated.")
+
+
+def extract_boundary(content_info: List[str]) -> Optional[bytes]:
+    """Extract boundary from content info."""
+    for item in content_info:
+        if '=' not in item:
+            continue
+
+        key, value = item.split('=', maxsplit=1)
+        if key.lower() == "boundary":
+            return b"--" + value.strip('"').encode("utf-8")
+
+    return None
